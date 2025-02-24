2131965b40ed8cacd3619f179c284513e311588f
mohamedfahimyousuf <mohamedfahimyousuf786@gmail.com>
2025-02-24 23:02:36 +0530

    To find all Divisors of a number Program

---
+++ AllDivisors.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class AllDivisors
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a Number: ");
+		int num = sc.nextInt();
+		System.out.println("All Divisors of the given Number is: ");
+		for(int i =1;i<=num;i++){
+		    if(num%i==0){
+		        System.out.print(i+" ");
+		    }
+		}
+        sc.close();
+	}
+}
