19a7032cb2ddea121937ed0bce24714a9d327d40
Mohamed Fahim M <129580983+mohamedfahimyousuf@users.noreply.github.com>
2025-03-06 21:12:09 +0530

    Update AllDivisors.java

--- AllDivisors.java
+++ AllDivisors.java
@@ -3,7 +3,7 @@ public class AllDivisors
 {
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
-		System.out.print("Enter a Number: ");
+		System.out.print("Enter a Number : ");
 		int num = sc.nextInt();
 		System.out.println("All Divisors of the given Number is: ");
 		for(int i =1;i<=num;i++){
