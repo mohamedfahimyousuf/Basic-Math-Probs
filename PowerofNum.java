b530b329b0374c8fc2a9d78aa8251fa47a862e24
Mohamed Fahim M <129580983+mohamedfahimyousuf@users.noreply.github.com>
2025-03-08 14:14:56 +0530

    Update PowerofNum.java

--- PowerofNum.java
+++ PowerofNum.java
@@ -3,9 +3,9 @@ public class PowerofNum
 {
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
-		System.out.print("Enter base: ");
+		System.out.print("Enter base : ");
 		int base = sc.nextInt();
-		System.out.print("Enter Exponent: ");
+		System.out.print("Enter Exponent : ");
 		int exponent = sc.nextInt();
 		int pow = 1;
 		for(int i=0;i<exponent;i++){
