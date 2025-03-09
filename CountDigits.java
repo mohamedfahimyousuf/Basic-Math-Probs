5e9fb6c8e33dd0bbdd7469ef9dc5985defc27e0d
Mohamed Fahim M <129580983+mohamedfahimyousuf@users.noreply.github.com>
2025-03-09 12:34:36 +0530

    Update CountDigits.java

--- CountDigits.java
+++ CountDigits.java
@@ -3,7 +3,7 @@ public class CountDigits
 {
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
-		System.out.print("Enter a Number: ");
+		System.out.print("Enter a Number : ");
 		int num = sc.nextInt();
 		int count = 0;
 		if(num==0){
