9be4b4c4aedc892c4f1bcadbf1137084ae04d617
Mohamed Fahim M <129580983+mohamedfahimyousuf@users.noreply.github.com>
2025-03-11 09:55:10 +0530

    Update LastDigit.java

--- LastDigit.java
+++ LastDigit.java
@@ -3,7 +3,7 @@ public class LastDigit
 {
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
-		System.out.print("Enter a Number: ");
+		System.out.print("Enter a Number : ");
 		int n = sc.nextInt();
 		System.out.println("The last Digit of the Number is: "+ n%10);
         sc.close();
