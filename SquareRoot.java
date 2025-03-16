fd1a9662a380f3a3cde5381873c01de89d881d25
Mohamed Fahim M <129580983+mohamedfahimyousuf@users.noreply.github.com>
2025-03-16 15:05:33 +0530

    Update SquareRoot.java

--- SquareRoot.java
+++ SquareRoot.java
@@ -3,7 +3,7 @@ public class SquareRoot
 {
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a Number: ");
+		System.out.println("Enter a Number : ");
 		double num = sc.nextDouble();
 		int sqrt=1;
 		for(int i=1;i*i<=num;i++){
